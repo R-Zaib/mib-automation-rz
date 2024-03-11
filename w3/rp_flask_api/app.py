9eaf6d6642ce2073b45961caf87d209605a176f8
Don <don@Dons-MacBook-Air.local>
2024-03-11 17:02:47 +0100

    modified the app.py and added home2.html for learning purposes

--- w3/rp_flask_api/app.py
+++ w3/rp_flask_api/app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import render_template # Remove: import Flask
+from flask import render_template, request # Remove: import Flask
 import connexion
 
 app = connexion.App(__name__, specification_dir="./")
@@ -9,6 +9,11 @@ app.add_api("swagger.yml")
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/fish")
+def fish():
+    return render_template("home2.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
